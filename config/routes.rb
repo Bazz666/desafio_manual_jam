f981552a448c1378cfa6c6afd3226a23ae360b02
bazz666 <herrera.se1992@gmail.com>
2021-06-07 17:08:35 -0400

    add users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  get 'users/destroy'
+
   resources :stories
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
