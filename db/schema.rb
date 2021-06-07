059fe46f5740ccb1c5f2de41859083dd27fa6e3d
bazz666 <herrera.se1992@gmail.com>
2021-06-07 17:58:09 -0400

    add f key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210607201610) do
+ActiveRecord::Schema.define(version: 20210607215601) do
 
   create_table "stories", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20210607201610) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_stories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,7 +28,7 @@ ActiveRecord::Schema.define(version: 20210607201610) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index :email, unique: true
+    t.string "index"
   end
 
 end
